eac2c4560b7d3c50f4065f44063ac4ca80dc35ec
Hrbtown <sixcasesixfile@gmail.com>
2022-08-27 14:42:35 +0530

    i have added a copy of print statement

--- add.c
+++ add.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){int a;
 printf("enter a number:\n");
+printf("enter a number:\n");
 scanf("%d",&a);
 (a>10)?printf("its greater than 10"):printf("its lesser than 10");
 }
